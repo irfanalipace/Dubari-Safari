41e56ba890f5f69112dc6c49dc3d930c9e74c957
Syed Mudasser Anayat <158043204+mudassirsaeedantechpvt@users.noreply.github.com>
2024-05-21 19:02:00 +0500

    New Changes

--- src/page/Search_Results/components/RightAside/RightAside.jsx
+++ src/page/Search_Results/components/RightAside/RightAside.jsx
@@ -1,7 +1,46 @@
+import { Box, Button, Typography } from "@mui/material";
 import React from "react";
-
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import SearchCard from "./components/SearchCard";
 const RightAside = () => {
-  return <div>This is Right Aside</div>;
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+  return (
+    <Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4" fontWeight="bold">
+          Tours Search Result
+        </Typography>
+        <FormControl sx={{ width: "200px" }}>
+          <InputLabel id="demo-simple-select-label">Recomended</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={age}
+            label="Recomended"
+            onChange={handleChange}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+      <SearchCard />
+    </Box>
+  );
 };
 
 export default RightAside;
